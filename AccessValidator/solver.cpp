48f3b41bfb2aa19d4fe29e071ac6845c1642de3f
sergadin <serg@msu.ru>
2018-04-21 15:50:25 +0300

    Initial version

--- AccessValidator/solver.cpp
+++ AccessValidator/solver.cpp
@@ -4,20 +4,7 @@
 #include <algorithm>
 #include <iterator>
 #include "AccessValidator.h"
-
-
-template<class TInputIterator, class T> inline
-bool contains(TInputIterator first, TInputIterator last, const T& value)
-{
-	return std::find(first, last, value) != last;
-}
-
-template<class TContainer, class T> inline
-bool contains(const TContainer& container, const T& value)
-{
-	return contains(std::begin(container), std::end(container), value);
-}
-
+#include "utils.h"
 
 typedef std::unordered_set<EquivalenceClass> History;
 
